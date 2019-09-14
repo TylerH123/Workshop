2a58a5c229fdc3ecca5fbdf36922128073c41f85
Tyler Huang <thuang00@stuy.edu>
2019-09-13 21:02:47 -0400

    convert to dictionary

---
+++ 03_csv/csv.py
@@ -0,0 +1,22 @@
+def copyToDict():
+    ary = []
+    dict = {}
+    file = open('../notes-and-code19-20/smpl/occupations.csv', 'r')
+    file.readline()
+    for line in file:
+        if '"' in line:
+            #print(line.split('"')[1:])
+            ary.append(line.split('"')[1:])
+        #print(line.split(',')[0])
+        else:
+            ary.append(line.split(','))
+    for arr in ary:
+        if ',' in arr[1]:
+            arr[1] = arr[1][1:]
+        #print(arr[1])
+        arr[1] = arr[1].replace('\n', '')
+        #print(arr)
+        dict.update({arr[0]:float(arr[1])})
+    print(dict)
+
+copyToDict()
