dc168ed347c92de23ff24e6f2a7d4a793fd55a80
Tyler Huang <thuang00@stuy.edu>
2019-09-18 09:21:48 -0400

    flask app with 3 routes

---
+++ 08_app0/flaskapp.py
@@ -0,0 +1,24 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def route1():
+    print(__name__)
+    return "first route"
+
+@app.route("/1")
+def route2():
+    print(__name__)
+    print(__name__)
+    return "second route"
+
+@app.route("/2")
+def route3():
+    print(__name__)
+    print(__name__)
+    print(__name__)
+    return "third route"
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
