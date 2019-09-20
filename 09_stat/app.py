187f552094a33897de76ef2c13a46d93776f8ed3
Tyler Huang <thuang00@stuy.edu>
2019-09-20 09:23:27 -0400

    template

--- 09_stat/app.py
+++ 09_stat/app.py
@@ -7,24 +7,6 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
-def route1():
-    print(__name__)
-    return "first route"
-
-@app.route("/1")
-def route2():
-    print(__name__)
-    print(__name__)
-    return "second route"
-
-@app.route("/2")
-def route3():
-    print(__name__)
-    print(__name__)
-    print(__name__)
-    return "third route"
-
 @app.route("/static")
 def helloWorld():
     print(__name__)
