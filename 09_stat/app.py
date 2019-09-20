510d6f08cb581a3685e22d1a3b3c48623e2e8f0b
Tyler Huang <thuang00@stuy.edu>
2019-09-20 09:21:12 -0400

    template + static

---
+++ 09_stat/app.py
@@ -0,0 +1,40 @@
+#teamFakeMoonLanding_HuangT_BhuiyanS
+#Tyler Huang
+#SoftDev1 pd2
+#K09: ’Tis Not a Race -- But You Will Go Faster
+#2019-09-18
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def route1():
+    print(__name__)
+    return "first route"
+
+@app.route("/1")
+def route2():
+    print(__name__)
+    print(__name__)
+    return "second route"
+
+@app.route("/2")
+def route3():
+    print(__name__)
+    print(__name__)
+    print(__name__)
+    return "third route"
+
+@app.route("/static")
+def helloWorld():
+    print(__name__)
+
+@app.route("/my_foist_template")
+def test_tmplt():
+    coll = [0, 1, 2, 3, 4, 5]
+    return render_template('my_foist_template.html', foo = "TeamFakeMoonLanding", collection = coll)
+    print(__name__)
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
