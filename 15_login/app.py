1601acc01f656ddd791e53aed6a2687734b51e75
Tyler Huang <thuang00@stuy.edu>
2019-10-03 19:56:40 -0400

    secret key

--- 15_login/app.py
+++ 15_login/app.py
@@ -6,6 +6,7 @@
 
 from flask import Flask, render_template, request, session, redirect, url_for
 app = Flask(__name__)
+app.secret_key = "bob123"
 
 login = {'user': 'Bob', 'pass': '123'}
 errMsg = ""
