0b52a225dd8ccac5b481f0483d01953537ddddc4
Tyler Huang <thuang00@stuy.edu>
2019-12-10 09:29:05 -0500

    gcd

--- 27_js/funky_town.js
+++ 27_js/funky_town.js
@@ -6,10 +6,23 @@ var fib = function(n){
   if (n == 0) {
     return 0;
   }
-  if (n <= 2) {
+  if (n == 1) {
     return 1;
   }
   else {
     return fib(n-1) + fib(n-2);
   }
+}
+
+var gcd = function(a,b){
+  if (b > a) {
+    return gcd(b,a);
+  }
+  var num,i;
+  for (i = 1; i < a; i++){
+    if (a % i == 0 && b % i == 0){
+      num = i;
+    }
+  }
+  return num;
 }
