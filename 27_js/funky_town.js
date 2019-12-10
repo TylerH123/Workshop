d04b07fba12075b67ea82cc9ccee06b49ccc7847
Tyler Huang <thuang00@stuy.edu>
2019-12-10 09:20:12 -0500

    js and fib

---
+++ 27_js/funky_town.js
@@ -0,0 +1,15 @@
+var func = function(n){
+  return n * 2;
+}
+
+var fib = function(n){
+  if (n == 0) {
+    return 0;
+  }
+  if (n <= 2) {
+    return 1;
+  }
+  else {
+    return fib(n-1) + fib(n-2);
+  }
+}
