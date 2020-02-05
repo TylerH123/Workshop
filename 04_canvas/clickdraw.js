5557012d37e302327fd9be57db402370902a2410
Tyler Huang <thuang00@stuy.edu>
2020-02-05 11:02:13 -0500

    rectangles

--- 04_canvas/clickdraw.js
+++ 04_canvas/clickdraw.js
@@ -1,18 +1,19 @@
 var c = document.getElementById("slate");
-//console.log(c);
 var ctx = c.getContext("2d");
-//console.log(ctx);
 ctx.fillStyle = "#ff0000";
-ctx.fillRect(50 , 50, 100, 200);
-//console.log(ctx);
 	
 var clear = function(){
-	var c = document.getElementById("slate");
-	var ctx = c.getContext("2d");
 	var width = c.width;
 	var height = c.height;
 	ctx.clearRect(0,0,width,height);
 }
 
+var createRect = function(){
+	var x = event.clientX;
+	var y = event.clientY;
+	ctx.fillRect(x , y, 100, 200);
+}
+
 var button = document.getElementById("clear");
 button.addEventListener("click", clear);
+c.addEventListener("click", createRect)
