31c0cf58a697ca0d8ad936a7140ae66750b7df1b
Tyler Huang <thuang00@stuy.edu>
2020-02-05 19:25:00 -0500

    push dots

--- 04_canvas/clickdraw.js
+++ 04_canvas/clickdraw.js
@@ -1,7 +1,19 @@
+/*
+William Lin, Tyler Huang
+SoftDev Pd. 2
+K #04: I See a Red Door...
+2020-02-05
+*/
+
 var c = document.getElementById("slate");
+//console.log(c);
 var ctx = c.getContext("2d");
+//console.log(ctx);
 ctx.fillStyle = "#ff0000";
-	
+// ctx.fillRect(50 , 50, 100, 200);
+// console.log(ctx);
+
+
 var clear = function(){
 	var width = c.width;
 	var height = c.height;
@@ -14,6 +26,43 @@ var createRect = function(){
 	ctx.fillRect(x , y, 100, 200);
 }
 
+var createDot = function(){
+  var x = event.clientX;
+	var y = event.clientY;
+	ctx.fillRect(x , y, 10, 10);
+}
+
+var shape = "rect";
+
+var toggle = document.getElementById("toggle");
+console.log(toggle.checked);
+
+toggle.addEventListener("click", function(){
+  console.log(toggle.isChecked);
+  if (toggle.checked){
+    shape = "dot";
+    document.getElementById("message").innerHTML = "Now drawing dots. Toggle to switch to rectangles."
+    // console.log("CHANGED TO CIRCLE");
+  }
+  else{
+    shape = "rect";
+    document.getElementById("message").innerHTML = "Now drawing rectangles. Toggle to switch to dots."
+    // console.log("testing");
+  }
+  // console.log("toggled");
+})
+
 var button = document.getElementById("clear");
 button.addEventListener("click", clear);
-c.addEventListener("click", createRect)
+
+c.addEventListener("click", function(){
+  if (shape == "rect"){
+    c.addEventListener("click", createRect());
+  }
+  else{
+    c.addEventListener("click", createDot());
+    console.log("creating DOT");
+  }
+  // console.log("clicked");
+  // console.log(shape);
+});
