eb400072f6c7f66a4b867533eb607dce363e98aa
Tyler Huang <thuang00@stuy.edu>
2020-02-13 11:59:34 -0500

    dvd bounce

--- 08_canvas-anim/animate.js
+++ 08_canvas-anim/animate.js
@@ -1,26 +1,30 @@
 /*
 Tyler Huang
 SoftDev Pd. 2
-K #07: They lock us in the tower whenever we get caught
-2020-02-12
+K #08: What is it saving the screen from?
+2020-02-20
 */
 
 var c = document.getElementById("slate");
 var ctx = c.getContext("2d");
 ctx.fillStyle = "#6a0dad";
-var shape = "rect";
-var id;
+var circleId, dvdId, dvdX, dvdY;
 var radius = 10;
 var growth = 1;
+var dirX = 1;
+var dirY = 1; 
+var logo = new Image();
+logo.src = "logo_dvd.jpg";
 
-var clear = function(){
-	var width = c.width;
-	var height = c.height;
-	ctx.clearRect(0,0,width,height);
+var clear = function(e){
+	e.preventDefault();
+	ctx.clearRect(0,0,c.width,c.height);
 }
 
-var animate = function(){
-	clear();
+var growCircle = function(){
+	window.cancelAnimationFrame(circleId);
+	window.cancelAnimationFrame(dvdId);
+	ctx.clearRect(0,0,c.width,c.height);
 	var x = c.width / 2;
 	var y = c.height / 2;
 	ctx.beginPath();
@@ -36,19 +40,45 @@ var animate = function(){
 	}
 	radius += growth;
 	//console.log(radius);
-	id = window.requestAnimationFrame(animate);
+	circleId = window.requestAnimationFrame(growCircle);
+}
+
+var displayDVD = function(){
+	ctx.clearRect(0,0,c.width,c.height);
+	dvdX = Math.round(Math.random() * (c.width - 250) + 125); 
+	dvdY = Math.round(Math.random() * (c.height - 250) + 125);
+	ctx.drawImage(logo,dvdX,dvdY,50,50);
+}
+
+var animateDVD = function(){
+	window.cancelAnimationFrame(circleId);
+	window.cancelAnimationFrame(dvdId);
+	ctx.clearRect(0,0,c.width,c.height);
+	dvdX += dirX;
+	dvdY += dirY;
+	//console.log(dvdX);
+	if (dvdX + 50 == c.width || dvdX + 2 == 0){
+		dirX = -1 * dirX;
+		//console.log(dvdX);
+	}
+	if (dvdY + 43 == c.height || dvdY + 7 == 0){
+		dirY = -1 * dirY;
+		//console.log(dvdY);
+	}
+	ctx.drawImage(logo,dvdX,dvdY);
+	dvdId = window.requestAnimationFrame(animateDVD);
+}
+
+var stop = function(){
+	window.cancelAnimationFrame(circleId);
+	window.cancelAnimationFrame(dvdId);
 }
 
 var stopButton = document.getElementById("stop");
-stopButton.addEventListener("click", function(e){
-	window.cancelAnimationFrame(id);
-	id = null;
-	}
-);
+stopButton.addEventListener("click", stop);
 
-var button = document.getElementById("animate");
-button.addEventListener("click", function(e){
-	if (id == null){
-		animate();
-	}}
-);
+var circle = document.getElementById("animate");
+circle.addEventListener("click", growCircle);
+
+var dvd = document.getElementById("waiting");
+dvd.addEventListener("click", function(e){displayDVD(); animateDVD()});
