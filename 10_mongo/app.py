006d42b6a7a402ca5ada3a4159d15bb8edb3f6eb
Tyler Huang <thuang00@stuy.edu>
2020-03-03 09:19:43 -0500

    finding senators by name

--- 10_mongo/app.py
+++ 10_mongo/app.py
@@ -13,6 +13,13 @@ client = MongoClient("mongodb://admin:thuang@64.225.14.222/")
 db = client.stickySulphur
 col = db.senators
 
-def findByBorough(borough):
-        for restaurant in restaurants.find({"borough": borough}):
-                print(restaurant["name"])
+def findByName(name):
+    if " " not in name: 
+        return("full name required")
+    else:
+        fname = name[:name.find(" ")]
+        lname = name[name.find(" ")+1:]
+    for senator in col.find({"person.firstname": fname, "person.lastname": lname}):
+        pprint(senator)
+
+findByName("Lamar Alexander")
